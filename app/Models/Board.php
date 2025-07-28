dc5c6cc3810bb58f41d89ece5f572449e853a28f
Ibrohim2442 <ibrohimavazov731@gmail.com>
2025-07-28 15:07:19 +0500

    add models, migrations, and relationships for projects, boards, statuses, and cards

---
+++ app/Models/Board.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Board extends Model
+{
+    /** @use HasFactory<\Database\Factories\BoardFactory> */
+    use HasFactory;
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+}
---
+++ app/Models/Status.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    /** @use HasFactory<\Database\Factories\StatusFactory> */
+    use HasFactory;
+
+    public function board()
+    {
+        return $this->belongsTo(Board::class);
+    }
+
+    public function cards()
+    {
+        return $this->hasMany(Card::class);
+    }
+}
